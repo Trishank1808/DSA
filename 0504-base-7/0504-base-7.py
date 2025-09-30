53b5a612f23de7177f91d4ce636b1ddefeffe62a
kolanka Trishank <kolankatrishank@gmail.com>
2025-09-30 20:42:13 +0530

    Time: 0 ms (100%), Space: 17.8 MB (63.23%) - LeetHub

---
+++ 0504-base-7/0504-base-7.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0: return "0"
+        sign = '-' if num < 0 else ''
+        num = abs(num)
+        res = ''
+        while num:
+            res = str(num % 7) + res
+            num //= 7
+        return sign + res
