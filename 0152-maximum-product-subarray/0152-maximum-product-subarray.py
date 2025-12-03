c8b3b19ede51fb8e77d27744a2ad77d7e41aabc3
kolanka Trishank <kolankatrishank@gmail.com>
2025-12-03 20:44:56 +0530

    Time: 11 ms (53.05%), Space: 18.5 MB (17.54%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -8,7 +8,7 @@ class Solution:
                 continue
             temp=curMax*n
             curMax=max(n*curMax,n*curMin,n)
-            curMin=min(n*curMax,n*curMin,n)
+            curMin=min(temp,n*curMin,n)
             res=max(res,curMax)
         return res
 
